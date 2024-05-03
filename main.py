8809cb293eb9bff3b0d91b75b76d6803cca84dd2
pkw <parks602@naver.com>
2024-05-03 12:22:41 +0900

    a

--- main.py
+++ main.py
@@ -11,5 +11,9 @@ app = FastAPI()
 app.include_router(board_router.app, tags=["board"])
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def root():
+    return {"message": "Hello World"}}
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
